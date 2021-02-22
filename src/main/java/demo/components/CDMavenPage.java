44d7a8ad05094e1415eac8202848f44086657ec7
Hugi Thordarson <hugi@karlmenn.is>
2021-02-22 20:56:52 +0000

    Added the Maven page

---
+++ src/main/java/demo/components/CDMavenPage.java
@@ -0,0 +1,12 @@
+package demo.components;
+
+import com.webobjects.appserver.WOContext;
+
+import demo.app.CDComponent;
+
+public class CDMavenPage extends CDComponent {
+
+	public CDMavenPage( WOContext context ) {
+		super( context );
+	}
+}
