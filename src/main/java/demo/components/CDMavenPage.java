ffa783135154cab4f2df191968c41c111bd94b09
Hugi Thordarson <hugi@karlmenn.is>
2021-02-22 21:46:21 +0000

    Looks nice. Let's keep that CommonMark renderer

--- src/main/java/demo/components/CDMavenPage.java
+++ src/main/java/demo/components/CDMavenPage.java
@@ -1,5 +1,9 @@
 package demo.components;
 
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
 import com.webobjects.appserver.WOContext;
 
 import demo.app.CDComponent;
@@ -8,5 +12,12 @@ public class CDMavenPage extends CDComponent {
 
 	public CDMavenPage( WOContext context ) {
 		super( context );
+	}
+
+	public String renderedTestText() {
+		Parser parser = Parser.builder().build();
+		Node document = parser.parse( "This is *Sparta*" );
+		HtmlRenderer renderer = HtmlRenderer.builder().build();
+		return renderer.render( document );
 	}
 }
