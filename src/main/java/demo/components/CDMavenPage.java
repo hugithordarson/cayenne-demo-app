44a86c9017b3d4a46b044e13134b46a0fd84d4f3
Hugi Thordarson <hugi@karlmenn.is>
2021-02-22 22:37:40 +0000

    Everyone needs tables

--- src/main/java/demo/components/CDMavenPage.java
+++ src/main/java/demo/components/CDMavenPage.java
@@ -1,5 +1,11 @@
 package demo.components;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import org.commonmark.Extension;
+import org.commonmark.ext.gfm.tables.TablesExtension;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
@@ -15,9 +21,26 @@ public class CDMavenPage extends CDComponent {
 	}
 
 	public String renderedTestText() {
-		Parser parser = Parser.builder().build();
-		Node document = parser.parse( "This is *Sparta*" );
-		HtmlRenderer renderer = HtmlRenderer.builder().build();
+		final List<Extension> extensions = Arrays.asList( TablesExtension.create() );
+
+		final Parser parser = Parser.builder()
+				.extensions( extensions )
+				.build();
+
+		final HtmlRenderer renderer = HtmlRenderer.builder()
+				.extensions( extensions )
+				.build();
+
+		final Node document = parser.parse( readStringFromResource( "/pages/maven.md" ) );
 		return renderer.render( document );
+	}
+
+	/**
+	 * @return The named resource as a string. StockOverflow calls this "the stupid scanner trick". I love it.
+	 */
+	private static String readStringFromResource( final String resourcePath ) {
+		try( Scanner scanner = new Scanner( CDMavenPage.class.getResourceAsStream( resourcePath ), "UTF-8" ).useDelimiter( "\\A" )) {
+			return scanner.next();
+		}
 	}
 }
--- src/main/resources/pages/maven.md
+++ src/main/resources/pages/maven.md
@@ -1,1 +1,7 @@
-Hello there
+    Hello there
+    How awesome is this?
+
+| First Header  | Second Header |
+| ------------- | ------------- |
+| Content Cell  | Content Cell  |
+| Content Cell  | Content Cell  |
