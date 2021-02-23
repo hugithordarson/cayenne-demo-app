81bdbb473087c12b13afe731fe1f13fc45b4f0e3
Hugi Thordarson <hugi@karlmenn.is>
2021-02-23 08:56:41 +0000

    Nicer look for docs text

--- src/main/java/demo/components/CDMavenPage.java
+++ src/main/java/demo/components/CDMavenPage.java
@@ -6,11 +6,16 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.commonmark.Extension;
+import org.commonmark.ext.gfm.tables.TableBlock;
 import org.commonmark.ext.gfm.tables.TablesExtension;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.AttributeProvider;
+import org.commonmark.renderer.html.AttributeProviderContext;
+import org.commonmark.renderer.html.AttributeProviderFactory;
 import org.commonmark.renderer.html.HtmlRenderer;
 
 import com.webobjects.appserver.WOContext;
@@ -32,6 +37,19 @@ public class CDMavenPage extends CDComponent {
 
 		final HtmlRenderer renderer = HtmlRenderer.builder()
 				.extensions( extensions )
+				.attributeProviderFactory( new AttributeProviderFactory() {
+					@Override
+					public AttributeProvider create( AttributeProviderContext context ) {
+						return new AttributeProvider() {
+							@Override
+							public void setAttributes( Node node, String tagName, Map<String, String> attributes ) {
+								if( node instanceof TableBlock ) {
+									attributes.put( "class", "table table-striped table-condensed" );
+								}
+							}
+						};
+					}
+				} )
 				.build();
 
 		final Node document = parser.parse( readStringFromResource( "/pages/maven.md" ) );
--- src/main/resources/pages/maven.md
+++ src/main/resources/pages/maven.md
@@ -1,7 +1,23 @@
-    Hello there
-    How awesome is this?
+# Þetta er stuð
+
+## Þetta er líka stuð
+
+## En ég er ekki jafn viss um þetta
+
+	public static final String string() {
+		System.out.println( "Hahaha" );
+	}
+
+---
 
 | First Header  | Second Header |
 | ------------- | ------------- |
 | Content Cell  | Content Cell  |
 | Content Cell  | Content Cell  |
+
+---
+
+
+Þjóðarþýðingin
+
+Hvað er þetta?
